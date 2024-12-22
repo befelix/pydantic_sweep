4bf1eaa2cf26035d7910c3249e707ff4aac7118a
Felix Berkenkamp <fberkenkamp@gmail.com>
2024-12-22 19:30:58 +0100

    AutoAPI reference added to docs (#17)

--- docs/conf.py
+++ docs/conf.py
@@ -38,11 +38,13 @@ extensions = [
     "sphinx.ext.napoleon",
     "myst_parser",
     "sphinx_rtd_theme",
+    "autoapi.extension",
 ]
 
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
+autoapi_dirs = ["../src"]
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
