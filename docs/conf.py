fc398a74e0e2288116d277e700214d9f9edcd0b8
Felix Berkenkamp <fberkenkamp@gmail.com>
2024-12-27 19:35:16 +0100

    Remove static part

--- docs/conf.py
+++ docs/conf.py
@@ -85,4 +85,3 @@ autodoc_typehints = "signature"
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "sphinx_rtd_theme"
-html_static_path = ["_static"]
